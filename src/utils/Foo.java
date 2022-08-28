89aebb75fa23de9842f3b8b3f168a1a9e38374ba
Chris Wood <christopher.t.wood@gmail.com>
2022-08-28 15:02:52 -0400

    12.2.8

--- src/utils/Foo.java
+++ src/utils/Foo.java
@@ -4,6 +4,7 @@ public class Foo {
 	public static void main(String[]args) {
 		System.out.println("howdy");
 		System.out.println("Hi from Github");
+		System.out.println("Hi 'chris1' branch, from Eclipse");
 	}
 
 }
